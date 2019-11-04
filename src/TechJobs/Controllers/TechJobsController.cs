10e2e1d5e27872a408bd952830a7cc02325459ff
Roman Tranquil <roma7d@gmail.com>
2019-11-04 01:02:11 -0500

    Everything is working except for bonus mission 7

---
+++ src/TechJobs/Controllers/TechJobsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechJobs.Controllers
+{
+    public class TechJobsController : Controller
+    {
+
+        internal static Dictionary<string, string> actionChoices = new Dictionary<string, string>();
+
+        static TechJobsController()
+        {
+            actionChoices.Add("search", "Search");
+            actionChoices.Add("list", "List");
+        }
+
+        public override ViewResult View()
+        {
+            ViewBag.actionChoices = actionChoices;
+            return base.View();
+        }
+
+        public override ViewResult View(string viewName)
+        {
+            ViewBag.actionChoices = actionChoices;
+            return base.View();
+        }
+
+        virtual public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
